b47c3d0388192a1487d40ea16295dc6189316e6d
김병곤 <fharenheit@gmail.com>
2023-07-30 08:21:17 +0900

    기본 구성

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ Flask 기반 REST API 프로젝트 템플릿
 ```
 # pip3 install -r requirements
 ```
+
+## Run
+
+```
+# export FLASK_APP = app.py
+# export FLASK_ENV = development
+# export FLASK_DEBUG = 0
+# python3 -m flask run 
+```
+
+## Trouble Shootings
+
+* `Fatal Python error: init_stdio_encoding: failed to get the Python codec name of the stdio encoding`
+  * 윈도 환경에서 IntelliJ에서 에러가 발생하는 경우 Settings에서 Encoding으로 검색하여 UTF-8로 변경 
--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+import argparse
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
+@app.route('/api/generator')
+def generator():
+
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    parser = argparse.ArgumentParser(description='Flask Server')
+    parser.add_argument('--bind', help='Binding IP Address', default='0.0.0.0')
+    parser.add_argument('--port', help='Binding Port', default='8080')
+    parser.add_argument('--debug', help='Binding Port', default='True')
+    args = parser.parse_args()
+
+    app.run(host=args.bind, port=args.port, debug=args.debug)

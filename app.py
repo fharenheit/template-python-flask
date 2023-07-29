c90ec749e692be57093bca2f89d95494391a97c5
김병곤 <fharenheit@gmail.com>
2023-07-30 08:32:12 +0900

    기본 구성

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 import argparse
+import traceback
 
-from flask import Flask, jsonify
+from flask import Flask, request, Response
+from generator import generator
 
 app = Flask(__name__)
 
 
-@app.route('/api/generator')
-def generator():
-
-    return 'Hello World!'
+@app.route('/api/generator', methods=['POST'])
+def generate():
+    json = request.get_json()
+    try:
+        return Response(generator.generate(json), status=200, mimetype='application/json')
+    except Exception as e:
+        returnValue = {
+            'message': traceback.format_exc()
+        }
+        return Response(returnValue, status=500, mimetype='application/json')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Flask Server')
     parser.add_argument('--bind', help='Binding IP Address', default='0.0.0.0')
     parser.add_argument('--port', help='Binding Port', default='8080')
-    parser.add_argument('--debug', help='Binding Port', default='True')
+    parser.add_argument('--debug', help='Debug Mode', default='True')
     args = parser.parse_args()
 
     app.run(host=args.bind, port=args.port, debug=args.debug)

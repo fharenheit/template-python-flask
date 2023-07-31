b34eed1b5bc3528c52b1f1c5d3294fc7a40b08f4
김병곤 <fharenheit@gmail.com>
2023-07-31 22:18:36 +0900

    Configuration 추가

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 import argparse
 import os
 import traceback
+import yaml
 
 from flask import Flask, request, Response
 from loguru import logger
 
 from generator import generator
 
-loggingPath = os.environ.get("LOGGING_PATH", ".")
-
-logger.add(loggingPath + "/service_{time}.log")
-
 app = Flask(__name__)
 
-logger.info("Now starting Flask Server")
+print(os.environ)
+
+def init(config):
+    configurationPath = os.environ.get("CONF_PATH", config)
+    with open(configurationPath) as f:
+        conf = yaml.safe_load(f)
+
+    logger.add(conf['app']['logging-path'])
+
+    logger.info("Now starting Flask Server...")
+    logger.info("Configuration Path : {}".format(configurationPath))
 
 
 @app.route('/api/generator', methods=['POST'])
@@ -28,11 +35,15 @@ def generate():
         return Response(returnValue, status=500, mimetype='application/json')
 
 
+if __name__.endswith("app"):
+    init('application.yml')
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Flask Server')
-    parser.add_argument('--bind', help='Binding IP Address', default='0.0.0.0')
-    parser.add_argument('--port', help='Binding Port', default='8080')
+    parser.add_argument('--config', help='Configuration File Path', default='application.yml')
     parser.add_argument('--debug', help='Debug Mode', default='True')
     args = parser.parse_args()
+
+    init(args.config)
 
     app.run(host=args.bind, port=args.port, debug=args.debug)
